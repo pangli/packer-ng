f14bf42d71d2bd3012decb7c5786bfc1a60545ea
xiaobona <bona.xiao@edaili.com>
2019-03-12 14:34:07 +0800

    渠道

---
+++ 360_jiagu/360_jiagu_generate＿package.bat
@@ -0,0 +1,2 @@
+java -jar packer-ng-2.0.1.jar generate --channels=@360_markets.txt --output=360_outPackage %1
+pause;
---
+++ legu_jiagu/legu_jiagu_generate＿package.bat
@@ -0,0 +1,2 @@
+java -jar packer-ng-2.0.1.jar generate --channels=@legu_markets.txt --output=legu_outPackage %1
+pause;
